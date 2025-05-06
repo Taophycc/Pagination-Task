68da66de1260bc4476f06a1de99a0ad32af95736
Taofeek Kassim <taophyckassim@gmail.com>
2025-05-06 16:04:20 +0100

    Update script.js

--- pagination task/script.js
+++ pagination task/script.js
@@ -4,28 +4,6 @@ const table2 = document.querySelector('.table2');
 const page = document.querySelectorAll('.page');
 const arrow = document.querySelectorAll('.arrow');
 
-// for (let i = 0; i < page.length; i++)
-//   page[i].addEventListener('click', function () {
-//     if (i === 0) {
-//       table1.classList.remove('hidden');
-//       table2.classList.add('hidden');
-//     } else if (i === 1) {
-//       table1.classList.add('hidden');
-//       table2.classList.remove('hidden');
-//     }
-//   });
-
-// for (let i = 0; i < page.length; i++)
-//   arrow[i].addEventListener('click', function () {
-//     if (i === 0) {
-//       table1.classList.remove('hidden');
-//       table2.classList.add('hidden');
-//     } else if (i === 1) {
-//       table1.classList.add('hidden');
-//       table2.classList.remove('hidden');
-//     }
-//   });
-
 const pagination = function (index) {
   if (index === 0) {
     table1.classList.remove('hidden');
